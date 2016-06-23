27a9adf5ef18e042285e42b64835873abca96a0d
mmutahr-daitio <mmutahr@daitio.com>
2016-06-23 17:31:26 -0400

    Initial commit.

---
+++ marsRover.js
@@ -0,0 +1,8 @@
+// Mars Rover: A CodeKata implementation by Muhammed Mutahr
+// To develop an api that moves a rover around a grid
+// Rover recieves a starting point (x, y), a direction it is facing (N, S, E, W), and a character array of commands.
+// A function to move rover forward or backward (f, b)
+// A function to change direction (l, r)
+// Functionality for wrapping  from one edge of the grid to another. 
+// Obstacle detection, if a given sequence of commands encounters an obstacle, 
+//      the rover moves up to the last possible point and reports the obstacle.
